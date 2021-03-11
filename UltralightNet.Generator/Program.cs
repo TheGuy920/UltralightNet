f9c9511ccb5b5898d68b01a71446dcaca3cfda4b
SupinePandora43 <supinepandoraggg@gmail.com>
2021-03-11 21:31:12 +0500

    test

--- UltralightNet.Generator/Program.cs
+++ UltralightNet.Generator/Program.cs
@@ -1,60 +1,74 @@
-using CppSharp;
-using CppSharp.AST;
-using CppSharp.Generators;
+using Octokit;
+using PeNet;
+using SharpCompress.Archives;
+using SharpCompress.Archives.SevenZip;
+using SharpCompress.Readers;
+using System;
+using System.Collections.Generic;
 using System.IO;
 using System.IO.Compression;
 using System.Net;
+using System.Text.Json;
+using System.Threading.Tasks;
 
 namespace UltralightNet.Generator
 {
 	public static class Program
 	{
-		private const string UltralightApi = "https://github.com/ultralight-ux/Ultralight-API/archive/master.zip";
-
-		public static void Main(string[] args)
+		public static async Task Main(string[] args)
 		{
-			#region download headers
-			if (Directory.Exists("./Ultralight-API-master"))
-				Directory.Delete("./Ultralight-API-master", true);
-			WebRequest request = WebRequest.CreateHttp(UltralightApi);
-			WebResponse response = request.GetResponse();
+			GitHubClient client = new(new ProductHeaderValue("my-cool-app"));
+			var latestRelease = await client.Repository.Release.GetLatest("ultralight-ux", "Ultralight");
 
+			ReleaseAsset asset = null;
+			foreach (var _asset in latestRelease.Assets)
+			{
+				if (_asset.Name.EndsWith("win-x64.7z"))
+				{
+					asset = _asset;
+				}
+			}
+			if (asset is null) throw new Exception();
+
+			Console.WriteLine(asset.Url);
+			Console.WriteLine(asset.BrowserDownloadUrl);
+
+			WebRequest request = WebRequest.CreateHttp(asset.BrowserDownloadUrl);
+			WebResponse response = await request.GetResponseAsync();
 			Stream responseStream = response.GetResponseStream();
-			ZipArchive archive = new ZipArchive(responseStream, ZipArchiveMode.Read);
+			MemoryStream stream = new();
+			await responseStream.CopyToAsync(stream);
 
-			archive.ExtractToDirectory("./");
-			#endregion
-			//ClangSharpUltralightGenerator.Generate();
-			//return;
-			ConsoleDriver.Run(new UltralightLibrary());
-		}
-	}
-	internal class UltralightLibrary : ILibrary
-	{
-		public override void Postprocess(Driver driver, ASTContext ctx)
-		{
+			var archive = SevenZipArchive.Open(stream);
 
-		}
+			var entries = archive.Entries;
 
-		public override void Preprocess(Driver driver, ASTContext ctx)
-		{
+			SevenZipArchiveEntry seven_entry = null;
 
-		}
+			foreach(var entry in entries)
+			{
+				Console.WriteLine(entry.Key);
+				if (entry.Key == "bin/Ultralight.dll")
+				{
+					seven_entry = entry;
+					break;
+				}
+			}
 
-		public override void Setup(Driver driver)
-		{
-			DriverOptions options = driver.Options;
-			options.GeneratorKind = GeneratorKind.CSharp;
+			if (seven_entry is null) throw new Exception();
 
-			var module = options.AddModule("Ultralight");
-			module.IncludeDirs.Add("./Ultralight-API-master");
-			module.Headers.Add("Ultralight/String.h");
-			//module.Headers.Add("JavaScriptCore/JavaScript.h");
-		}
+			Stream ultralightDLLReadOnly = seven_entry.OpenEntryStream();
 
-		public override void SetupPasses(Driver driver)
-		{
+			MemoryStream ultralightDll = new();
 
+			await ultralightDLLReadOnly.CopyToAsync(ultralightDll);
+
+			PeFile peFile = new(ultralightDll);
+
+			foreach(var exported in peFile.ExportedFunctions)
+			{
+				Console.WriteLine(exported.Name);
+			}
 		}
 	}
 }
